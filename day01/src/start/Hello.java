dbf990b9d5f97654b8b2e27b16e8531e3d4bc0c5
swings134man <86291550+swings134man@users.noreply.github.com>
2021-06-22 18:05:01 +0900

    Add files via upload

---
+++ day01/src/start/Hello.java
@@ -0,0 +1,13 @@
+package start; //항상 맨위에는 package 이름이 와야한다.
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// main 함수의 처리내용을 넣어보자.
+		// syso, sysout + 컨트롤 + 스페이스바 
+		System.out.println("환영합니다.");
+		System.out.println("반갑습니다.");
+		System.out.println("나는 자동완성입니다.!!!");
+	}
+
+}
---
+++ day01/src/start/Hi.java
@@ -0,0 +1,12 @@
+package start;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		System.out.println("반가워");
+		System.out.println("안녕");
+		System.out.println("하이"); //prinln 에서 ln은 line 한줄 띄워줌
+
+	}
+
+}
